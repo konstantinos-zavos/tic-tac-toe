0124761d11850b5782c7e30aa8a532f42958bb72
koszav <zavos@tuta.io>
2023-01-24 20:40:18 +0200

    Add gameboard object with an array inside, test function that adds a string to the object 's array, and a sample player factory function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
-function gameboard = (() => {
-    
-})
+const gameBoard = (() => {
+  const gameBoard = ["X", "O", "X"];
+  return {
+    gameBoard
+  };
+})();
+
+function test() {
+    let testArray = "?";
+    gameBoard.gameBoard.push(testArray);
+}
+
+const Player = (name) => {
+    const sayName = () => console.log(`my name is ${name}`);
+    return {sayName};
+}
+
+//  I have created an array inside my object
+//  I have created a player object that just prints a name
+//  pushing items in the array inside the object
+//  gameBoard.gameBoard.push(testArray); This work with strings
+//  If you push an array it will push another array inside the array
