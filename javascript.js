05855220be75391a879005cd324c0ca97ef32701
koszav <zavos@tuta.io>
2023-01-26 13:28:30 +0200

    Add very basic dom manipulation on webpage elements

--- javascript.js
+++ javascript.js
@@ -6,9 +6,18 @@ const gameBoard = (() => {
 })();
 
 function test() {
-    let testArray = "?";
+    let testArray = "1";
     gameBoard.gameBoard.push(testArray);
     document.getElementById("arrPrint").innerHTML = JSON.stringify(gameBoard.gameBoard, null, 2);
+}
+
+
+function addMarkToBlockOne() {
+    const block1 = document.querySelector('.block-1');
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.textContent = 'X';
+    block1.appendChild(content);
 }
 
 const Player = (name) => {
@@ -24,4 +33,7 @@ jim.sayName();
 //  I have created a player object that just prints a name
 //  pushing items in the array inside the object
 //  gameBoard.gameBoard.push(testArray); This work with strings
-//  If you push an array it will push another array inside the array
+//  If you push an array it will push another array inside the array    
+// I want to create functions that insert X or O on DOM elements
+
+// Game should start with player 1 and X (white piece) and then O (black pieces)
