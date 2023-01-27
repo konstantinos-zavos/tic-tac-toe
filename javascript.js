ff8c81bdf2671575b7bed7e18439e7fee575003e
koszav <zavos@tuta.io>
2023-01-27 14:46:09 +0200

    Add comments that explain problems related to the project

--- javascript.js
+++ javascript.js
@@ -36,11 +36,27 @@ function addMarkToBlockOne() {
   block1.appendChild(content);
 }
 
-//  I have created an array inside my object
-//  I have created a player object that just prints a name
-//  pushing items in the array inside the object
-//  gameBoard.gameBoard.push(testArray); This work with strings
-//  If you push an array it will push another array inside the array
-// I want to create functions that insert X or O on DOM elements
 
+// I want to create functions that insert X or O on DOM elements
 // Game should start with player 1 and X (white piece) and then O (black pieces)
+
+
+
+
+// In short, break the big problem down and solve each of the smaller problems
+//  until you’ve solved the big problem.
+
+// Show player's turn
+// Object has player's names
+// Function acesses object and prints player's name on screen depending on turn
+
+// Show which player's turn it is in the top of the game board
+// PLayer 1 clicks on the board
+// X mark appears on the gameboard
+// Now it is player's 2 turn
+// Change active player's name on top of screen
+// Player 2 inputs his mark into the desired space and it appears
+// Now its players 1 turn again
+// Repeat until victory
+
+// Make sure only 1 spot is able to used per mark
