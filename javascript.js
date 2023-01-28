6b4ad93b2ccd80ea5522cfb4b9222a7ce2924d0b
koszav <zavos@tuta.io>
2023-01-28 19:31:18 +0200

    Add a new function adds player's symbol on the 4th index on click

--- javascript.js
+++ javascript.js
@@ -1,50 +1,73 @@
 /* eslint-disable no-console */
 // GameBoard Module (one of something)
 
+const Player = (name, symbol) => {
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  const saySymbol = () => console.log(`my symbol is ${symbol}`);
+  const sayName = () => console.log(`my name is ${name}`);
+  return { sayName, getName, getSymbol, saySymbol };
+};
+
+const jim = Player("Jim", "no");
+jim.sayName();
+jim.saySymbol();
+
+// function test() {
+//   const playerToken = jim.getSymbol();
+//   gameBoard.board[1] = playerToken;
+// }
+
 const gameBoard = (() => {
   const board = ["X", "O", "X", "O", "X", "X", "O", "X", "X"];
-  const generateBoard = () => { 
+  const generateBoard = () => {
     let i = 0;
     while (i < board.length) {
-      console.log(board[i]);
       const container = document.querySelector(".game-container");
       const block = document.createElement("div");
       block.id = "block";
       block.setAttribute("class", `block-${i}`);
       block.textContent = board[i];
+      block.setAttribute("onclick", "gameBoard.test()");
       container.appendChild(block);
       i += 1;
     }
   };
+  const test = () => {
+    scoped();
+    // const playerToken = jim.getSymbol();
+    // gameBoard.board[1] = playerToken;
+  };
   return {
     board,
     generateBoard,
+    test,
   };
 })();
 
-gameBoard.generateBoard();
-
-function placeMark() {
-  const btn = document.querySelector('#btn');
-  btn.addEventListener('click', () => {
-  console.log("Hello World");
-  });
+function scoped() {
+  const playerToken = jim.getSymbol();
+  gameBoard.board[3] = playerToken;
+  const container = document.querySelector(".game-container");
+  container.replaceChildren();
+  gameBoard.generateBoard();
 }
 
+gameBoard.generateBoard();
 
+// function AddMarkOnBlock() {
+//   const divName
+//   if (divName = "")
+// }
 
-//  same idea from below : (First create loop that generates a div from each array string) then --I want to generate grid on DOM from array, I already have the CSS ready)
+// When player click on block-1
+// gameBoard.board[1] = activePlayer.symbol;
+
+// WHen user click on dom element, get user.token (which is X or O) and gameBoard.board[1] = user.token
+
 // idea : if player1 click gameboard.board[3] then show X on DOM-box-[3] then change turn to player 2
 
 // Player Factory function (multiple of something)
-// const Player = (name) => {
-//   const getName = () => name;
-//   const sayName = () => console.log(`my name is ${name}`);
-//   return { sayName, getName };
-// };
-
-// const jim = Player("Jim");
-// jim.sayName();
 
 // function test() {
 //   const testArray = "1";
